71bd9ed9fe72e2b03ad6d01eceec2c34797c57ac
sakthi <developersakthi23@gmail.com>
2023-12-07 14:08:30 +0530

    Filter Functionalities

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { ScrollTopModule } from 'primeng/scrolltop';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { DropdownModule } from 'primeng/dropdown';
+import { HeaderComponent } from '../header/header.component';
 
 interface UserData {
   id: number;
@@ -36,6 +37,7 @@ interface UserData {
     NzInputModule,
     NzButtonModule,
     DropdownModule,
+    HeaderComponent,
     
   ],
   templateUrl: './home.component.html',
@@ -45,15 +47,38 @@ export class HomeComponent {
   Users: UserData[] = studentsData;  
   searchName: string = '';
   filteredPeople: UserData[] = [];
-
+  Filters: any[] = [
+    { name: "Domain" },
+    { name: "Gender" },
+    { name: "Availability" }
+  ];
+  selectedFilter:any;
+  filter:boolean=false;
+  filteredUsers: UserData[] = [];
+  domainFilter: string = '';
+  genderFilter: string = '';
+  availabilityFilter: boolean | null = null;
   paginatedUsers: any[] = []; 
   totalUsers: number = 0; 
   rows: number = 20;
-  constructor() {
-
+  uniqueDomains: string[] = [];
+  selectedTeam: any[] = [];
+  teams: any[] = [];
+  filteredCards: any[] = [];
+  uniqueGenders:string[]=[];
+  constructor() {    
     this.totalUsers = this.Users.length;
     this.paginatedUsers = this.Users.slice(0, this.rows);
   }
+  ngOnInit(): void {
+    this.findUniqueDomains();
+  }
+  findUniqueDomains() {
+    this.uniqueDomains = [...new Set(this.Users.map((card: any) => card.domain))];
+    this.uniqueGenders = [...new Set(this.Users.map((card: any) => card.gender))];
+
+  }
+
 
   onPageChange(event: any) {
     const startIndex = event.first;
@@ -64,6 +89,39 @@ export class HomeComponent {
       person.first_name.toLowerCase().includes(this.searchName.toLowerCase()) ||
       person.last_name.toLowerCase().includes(this.searchName.toLowerCase())
     );
+    this.filter=true;
     console.log(this.filteredPeople)
+  }
+  clear() {
+    this.searchName=""
+    this.filter=false;
+    console.log(this.filteredPeople)
+  }
+
+  filterByDomain(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    if (selectElement) {
+      const domain = selectElement.value;
+      this.filteredCards = this.Users.filter((card: any) => (domain ? card.domain === domain : true));
+    }
+  }
+
+  filterByGender(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    if (selectElement) {
+      const gender = selectElement.value;
+      this.filteredCards = this.Users.filter((card: any) => (gender ? card.gender === gender : true));
+    }
+  }
+
+  filterByAvailability(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    if (selectElement) {
+      const availability = selectElement.value;
+      const available = availability === 'true' ? true : availability === 'false' ? false : null;
+      this.filteredCards = this.Users.filter((card: any) =>
+        available !== null ? card.available === available : true
+      );
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 /// <reference types="@angular/localize" />
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
+import { InputGroup } from 'primeng/inputgroup';
+import { InputGroupAddon } from 'primeng/inputgroupaddon';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
